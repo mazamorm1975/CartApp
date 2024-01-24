b4a7801757cc0e8be2dbb80812719d820076468e
mazamora1975 <mzamora114@hotmail.com>
2024-01-24 12:56:34 -0600

    Modulo 78. Maquetando productos HTML y CSS.

---
+++ src/CartApp.jsx
@@ -0,0 +1,82 @@
+export const CartApp = () => {
+  return (
+    <>
+      <h1>Cart App</h1>
+      <div className="row">
+        <div className="col-4 my-2">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Teclado Mecanico RGB</h5>
+              <p className="card-text">
+                Teclado Mecanico con luces RGB switches cherry red
+              </p>
+              <p className="card-text">$ 5,000.45</p>
+              <button className="btn btn-primary">Agregar</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-4 my-2">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Samsung Smart TV</h5>
+              <p className="card-text">
+                Forma Hergonomica para LapTop
+              </p>
+              <p className="card-text">$ 16,000.85</p>
+              <button className="btn btn-primary">Agregar</button>
+            </div>
+          </div>
+        </div>
+        <div className="col-4 my-2">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Audifono Bluetooth Sony</h5>
+              <p className="card-text">
+                Sincronización via Wi-Fi
+              </p>
+              <p className="card-text">$ 800.42</p>
+              <button className="btn btn-primary">Agregar</button>
+            </div>
+          </div>
+        </div>
+        <div className="col-4 my-2">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Memoria Corsair 8GB DDR5</h5>
+              <p className="card-text">
+                Expansion a 32GB
+              </p>
+              <p className="card-text">$ 169.78</p>
+              <button className="btn btn-primary">Agregar</button>
+            </div>
+          </div>
+        </div>
+        <div className="col-4 my-2">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Asus Nvidia RTX</h5>
+              <p className="card-text">
+                Activado por voz inteligente
+              </p>
+              <p className="card-text">$ 1,200.00</p>
+              <button className="btn btn-primary">Agregar</button>
+            </div>
+          </div>
+        </div>
+        <div className="col-4 my-2">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">CPU Intel Core i5</h5>
+              <p className="card-text">
+                7ma. Generación
+              </p>
+              <p className="card-text">$ 40,000.95</p>
+              <button className="btn btn-primary">Agregar</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
