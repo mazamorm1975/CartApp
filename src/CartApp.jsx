55927663adc793e02387d09f331f02ea8380359e
mario <mzamora114@hotmail.com>
2024-02-09 13:18:48 -0600

    84. Manejando el estado del carro de compras

--- src/CartApp.jsx
+++ src/CartApp.jsx
@@ -1,14 +1,35 @@
+import { useState } from "react";
 import { CartView } from "./Components/CartView";
 import { CatalogView } from "./Components/CatalogView";
 
+const initialCartItems = [
+/*
+  {
+    product: {
+      id:1,
+        name:"Teclado Mecanico RGB",
+        description:"Teclado Mecanico con luces RGB switches cherry red",
+        price: 5000.45
+    },
+    quantity: 0,
+    total: 0
+  }
+*/
+]
+
+
+
 export const CartApp = () => {
-    return (
+
+  const [cartItems, setCartItem] = useState(initialCartItems);
+
+  return (
     <>
       <div className="container">
         <h1>Cart App</h1>
-          <CatalogView/>
+        <CatalogView />
         <div className="my-4 w-50">
-          <CartView/>
+          <CartView items={cartItems}/>
         </div>
       </div>
     </>
