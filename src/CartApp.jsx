6e2d33b47bdcc1a97749a6fb848f6d731cd1e8d6
Mario Zamora <mzamora114@hotmail.com>
2025-02-04 13:02:19 -0600

    Se modifica clase CartApp.jsx

--- src/CartApp.jsx
+++ src/CartApp.jsx
@@ -1,8 +1,5 @@
-
-import { CartView } from "./Components/CartView";
-import { CatalogView } from "./Components/CatalogView";
 import { Navbar } from "./Components/Navbar";
-import { useItemCart } from "./hooks/useItemCart";
+import {useItemCart} from "./hooks/useItemCart"
 import { CartRoutes } from "./routes/CartRoutes";
 
 //De la linea 7 - 16 se usa como test la opcion Fetch de jsonplaceholder para testeo de una API's devolviendo un json
@@ -17,54 +14,25 @@ httpClient.then(response => {
 
 });
 
+
+
 export const CartApp = () => {
 
-  const { cartItems, handlerAddProductCart, handlerDeleteProductCart } = useItemCart();
+    const { cartItems, handlerAddProductCart, handlerDeleteProductCart} = useItemCart();
+    
+    return (
+        <>
+            <Navbar />
+            <div className="container my-4">
 
-  return (
-    <>
-      <Navbar/>
-      <div className="container my-4">
-        <h1>Cart App</h1>
+                <h3>Cart App</h3>
+                <CartRoutes
+                    cartItems={cartItems}
+                    handlerAddProductCart={handlerAddProductCart}
+                    handlerDeleteProductCart={handlerDeleteProductCart}
+                />
+            </div>
+        </>
+    )
+}
 
-        <Routes>
-
-          <Route path="catalog"
-            element={<CatalogView handler={product => handlerAddProductCart(product)} />} />
-
-          <Route path="cart" element={(
-            cartItems?.length <= 0 ?
-              <div className="alert alert-warning">No hay elementos para mostrar</div> :
-              (
-                <div className="my-4 w-50">
-                  <CartView items={cartItems} handlerDelete={handlerDeleteProductCart} />
-                </div>
-              )
-          )} />
-
-          <Route path="/" element={<Navigate to={'/catalog'} />} />
-
-        </Routes>
-
-
-
-      </div>
-    </>
-  );
-
-  return (
-    <>
-      <div className="container my-4">
-        <h1>Cart App</h1>
-        <CatalogView handler={product => handlerAddProductCart(product)} />
-
-        {cartItems?.length <= 0 || (
-          <div className="my-4 w-50">
-            <CartView items={cartItems} handlerDelete={handlerDeleteProductCart} />
-          </div>
-        )}
-      </div>
-    </>
-  );
-
-};
